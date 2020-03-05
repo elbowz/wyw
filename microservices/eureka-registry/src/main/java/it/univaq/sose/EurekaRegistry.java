e75a64d8894932aa41ba08fc4afc5905430a7796
Emanuele Palombo <muttley.bd@gmail.com>
2020-03-05 17:55:57 +0100

    Add Eureka registry server

---
+++ microservices/eureka-registry/src/main/java/it/univaq/sose/EurekaRegistry.java
@@ -0,0 +1,20 @@
+package it.univaq.sose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaRegistry extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaRegistry.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegistry.class, args);
+    }
+}
