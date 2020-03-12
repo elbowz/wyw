fb979b0135debdc9b9484b132fb6c0a862a94a8d
Emanuele Palombo <muttley.bd@gmail.com>
2020-03-12 15:48:07 +0100

    Add Zuul proxy server

--- microservices/zuul/README.md
+++ microservices/zuul/README.md
@@ -1,1 +1,3 @@
-# Zuul proxy server.
+# Zuul proxy server
+
+For check it works try to call `http://localhost:8762/hello/sose/helloservice/sayHello/666` 
--- microservices/zuul/src/main/java/it/univaq/sose/ZuulProxyApplication.java
+++ microservices/zuul/src/main/java/it/univaq/sose/ZuulProxyApplication.java
@@ -2,13 +2,19 @@ package it.univaq.sose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
-public class ZuulProxyApplication {
+public class ZuulProxyApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ZuulProxyApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulProxyApplication.class, args);
