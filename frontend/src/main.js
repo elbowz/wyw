7da147340e56b3aad6006295ff087c3f6400d46e
Emanuele Palombo <muttley.bd@gmail.com>
2020-04-27 16:58:16 +0200

    Add custom and themed Bootstrap
    Also animate.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue';
+// TODO: add only the used modules (eg. import { AlertPlugin } from 'bootstrap-vue')
+// see: https://bootstrap-vue.js.org/docs#component-groups-and-directives-as-vue-plugins
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+
 import App from './App.vue';
 import router from './router';
+
+import './styles/index.scss';
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
