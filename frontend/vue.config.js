e05c27b7d52c663e684cea23f638a91f8a3efd76
Emanuele Palombo <muttley.bd@gmail.com>
2020-05-01 22:29:29 +0200

    Set SPA title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   lintOnSave: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'WhatYouWatched - Service Oriented Software Engineering 2019/20',
+    },
+  },
 };
