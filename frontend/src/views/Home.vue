74f09391cbf184f145385e2daaf003bf2b007978
Emanuele Palombo <muttley.bd@gmail.com>
2020-05-05 19:39:55 +0200

    Add Home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-jumbotron bg-variant="white" class="text-center" fluid>
+    <template v-slot:header>
+      <div class="d-flex justify-content-center"></div>
+      <div class="logo-wyw mx-auto"></div>
+      <h1><strong>W</strong>hat<strong>Y</strong>ou<strong>W</strong>atched</h1>
+    </template>
+
+    <template v-slot:lead>
+      Keep track what you and other people watching
+    </template>
+
+    <hr class="my-4">
+
+    <p class="font-weight-light">
+      You are able to find new films and tv series by exploring users timelines and rating by the most famous internet aggregator
+    </p>
+
+    <b-button variant="outline-dark" :to="{ name: 'login' }">Login to Start</b-button>
+  </b-jumbotron>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
