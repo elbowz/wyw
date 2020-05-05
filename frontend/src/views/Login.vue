9df3f9274d8f1f84a97708906c0652a48eac5272
Emanuele Palombo <muttley.bd@gmail.com>
2020-05-05 19:40:19 +0200

    Restyle the Login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-    <form @submit.prevent="login">
-      <h1>Sign in</h1>
-      <label>email</label>
-      <input required v-model="email" type="text" placeholder="email@domain.com"/>
-      <label>Password</label>
-      <input required v-model="password" type="password" placeholder="Password"/>
-      <button type="submit">Login</button>
-    </form>
-    <div v-if="error" class="error">
-      {{ error }}
-    </div>
-  </div>
+  <b-row class="text-center mb-3" style="padding-top: 80px;">
+    <b-col md="4" class="mx-md-auto">
+      <h1><b-icon icon="people-circle" aria-label="Login" variant="dark" font-scale="2"></b-icon></h1>
+      <h3 class="mb-4">Sign in</h3>
+
+      <form @submit.prevent="login">
+        <div class="mb-2">
+          <b-form-input v-model="email"
+                        :state="errorState"
+                        type="email" aria-describedby="input-email-help " placeholder="email@domain.com"
+                        autofocus required></b-form-input>
+          <!-- <b-form-text id="input-email-help">email address used for registration</b-form-text>-->
+        </div>
+        <div class="mb-3">
+          <b-form-input v-model="password"
+                        :state="errorState"
+                        type="password"
+                        aria-describedby="input-password-help input-password-feedback" placeholder="password"
+                        required></b-form-input>
+          <!--<b-form-text id="input-password-help">at least 6 characters</b-form-text>-->
+          <b-form-invalid-feedback id="input-password-feedback">{{ error }}</b-form-invalid-feedback>
+        </div>
+
+        <b-button variant="outline-dark" type="submit" block>Login</b-button>
+      </form>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -36,6 +50,11 @@ export default {
         .catch((error) => {
           this.error = 'username or password are wrong!';
         });
+    },
+  },
+  computed: {
+    errorState() {
+      return this.error == null ? null : false;
     },
   },
 };
