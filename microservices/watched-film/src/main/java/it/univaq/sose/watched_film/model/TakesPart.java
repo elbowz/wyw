39857a2f633d3ef637fb5a19d4f0be48599affbf
Emanuele Palombo <muttley.bd@gmail.com>
2020-05-10 13:46:46 +0200

    Update person wsdl and related models update

--- microservices/watched-film/src/main/java/it/univaq/sose/watched_film/model/TakesPart.java
+++ microservices/watched-film/src/main/java/it/univaq/sose/watched_film/model/TakesPart.java
@@ -1,23 +1,10 @@
 package it.univaq.sose.watched_film.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class TakesPart {
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private TakesPartKey id;
-
     private String role;
     private Person person;
 
     public TakesPart() {
-    }
-
-    public TakesPartKey getId() {
-        return id;
-    }
-
-    public void setId(TakesPartKey id) {
-        this.id = id;
     }
 
     public String getRole() {
@@ -39,7 +26,6 @@ public class TakesPart {
     @Override
     public String toString() {
         return "TakesPart{" +
-                "id=" + id +
                 ", role='" + role + '\'' +
                 ", person=" + person +
                 '}';
