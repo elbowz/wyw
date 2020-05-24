59459ec993059e81f60b0b637ca26ba3f07c96ac
Emanuele Palombo <muttley.bd@gmail.com>
2020-05-25 00:50:41 +0200

    Move poster under /img

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:8762';
-export const CDN_URL = process.env.VUE_APP_CDN_URL || `${process.env.BASE_URL}imgs/`;
+export const CDN_URL = process.env.VUE_APP_CDN_URL || `${process.env.BASE_URL}img/`;
 
 export default API_URL;
